a729ab5e3941985bbdf6681c6a83d96af1f75b7e
for { kys() } <yyeet62425@gmail.com>
2024-10-12 11:58:15 +0530

    move to astro

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "hybrid",
+  site: "https://0x-kys.github.io",
+  base: "0x-kys.github.io",
+});
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}", // Add paths where Tailwind should look for class names
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
