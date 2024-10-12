288551448c717da736d1de91ec0bee2d06be8ae5
for { kys() } <yyeet62425@gmail.com>
2024-10-12 12:51:25 +0530

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
 
-// https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
+  // output: "hybrid",
   site: "https://0x-kys.github.io",
-  // base: "0x-kys.github.io",
-  //
-  // no need to set base if project is being loaded from
-  // root or repo name is in <username>.github.io format
 
-  adapter: node({
-    mode: "standalone",
-  }),
+  // adapter: node({
+  //   mode: "standalone",
+  // }),
 });
