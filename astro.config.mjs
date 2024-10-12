0e62e1eaa6be607e7bcd6b33f2a72eb6effe173b
for { kys() } <yyeet62425@gmail.com>
2024-10-12 12:05:36 +0530

    fix build fail because of no adapters

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   site: "https://0x-kys.github.io",
   base: "0x-kys.github.io",
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
