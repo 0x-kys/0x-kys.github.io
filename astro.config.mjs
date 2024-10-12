5bd2b96eaeab9657d304269df0a588113d5be2f1
for { kys() } <yyeet62425@gmail.com>
2024-10-12 12:12:50 +0530

    fix build fail

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,10 @@ import node from "@astrojs/node";
 export default defineConfig({
   output: "hybrid",
   site: "https://0x-kys.github.io",
-  base: "0x-kys.github.io",
+  // base: "0x-kys.github.io",
+  //
+  // no need to set base if project is being loaded from
+  // root or repo name is in <username>.github.io format
 
   adapter: node({
     mode: "standalone",
